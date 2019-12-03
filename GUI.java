5a1c983689f10ffa0ed8b97eeee5f66c782a6a84
unknown <dustin36@naver.com>
2019-12-03 12:53:53 +0900

    20191203

--- GUI.java
+++ GUI.java
@@ -3,6 +3,7 @@ package javaTermProjectPackage;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.io.IOException;
 import javax.swing.*;
 import javax.swing.table.*;
@@ -40,9 +41,22 @@ public class GUI extends JFrame {
 	private Icon trash2Enter = new ImageIcon(GUI.class.getResource("../image/Comp 1_00006.png"));
 	private Icon trash2Exit = new ImageIcon(GUI.class.getResource("../image/Comp 1_00007.png"));
 	private JButton trash2 = new JButton(new ImageIcon(GUI.class.getResource("../image/Comp 1_00007.png")));
+	private Icon printEnter = new ImageIcon(GUI.class.getResource("../image/Comp 1_00004.png"));
+	private Icon printExit = new ImageIcon(GUI.class.getResource("../image/Comp 1_00005.png"));
+	private JButton print = new JButton(new ImageIcon(GUI.class.getResource("../image/Comp 1_00005.png")));
+	private Icon dupEnter = new ImageIcon(GUI.class.getResource("../image/Comp 2_00006.png"));
+	private Icon dupExit = new ImageIcon(GUI.class.getResource("../image/Comp 2_00007.png"));
+	private JButton dup = new JButton(new ImageIcon(GUI.class.getResource("../image/Comp 2_00007.png")));
+	private Icon moveEnter = new ImageIcon(GUI.class.getResource("../image/Comp 4_00000.png"));
+	private Icon moveExit = new ImageIcon(GUI.class.getResource("../image/Comp 4_00001.png"));
+	private JButton move = new JButton(new ImageIcon(GUI.class.getResource("../image/Comp 4_00001.png")));
 	private ArrayList<List> listAry;
 	private DefaultTableModel model;
 	private int listIdx;
+	private int listIdx2;
+	private String listNameField;
+	private Container c1;
+	private Container c2;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -54,16 +68,18 @@ public class GUI extends JFrame {
 	}
 
 	public GUI() {
+	
 		setTitle("MyShoppingList");
 		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 		setResizable(false);
 		setVisible(true);
 		setLocationRelativeTo(null);
 		setLayout(null);
-		Start st = new Start();
 
-		Container c = getContentPane();
-		c.setLayout(null);
+		//Container c = getContentPane();
+		//c.setLayout(null);
+		
+		Start st = new Start();
 
 		listAry = st.getList();
 		System.out.println(listAry);
@@ -79,15 +95,42 @@ public class GUI extends JFrame {
 				System.exit(0);
 			}
 		});
+		//d1
+		AddListListener addListListener = new AddListListener();
+		addList.addMouseListener(addListListener);
+		
+		TodayListener todayListener = new TodayListener();
+		today.addMouseListener(todayListener);
+		
+		BookMarkListener bookMarkListener = new BookMarkListener();
+		bookMark.addMouseListener(bookMarkListener);
+		
+		TrashListener trashListener = new TrashListener();
+		trash.addMouseListener(trashListener);
+		//d2
+		AddItemListener addItemListener = new AddItemListener();
+		addItem.addMouseListener(addItemListener);
+		
+		HomeListener homeListener = new HomeListener();
+		home.addMouseListener(homeListener);
+		
+		Trash2Listener trash2Listener = new Trash2Listener();
+		trash2.addMouseListener(trash2Listener);
+		
+		MoveListener moveListener = new MoveListener();
+		move.addMouseListener(moveListener);
 
 		d1(listAry);
 	}
 	
-	public void d1(ArrayList<List> listAry) {
+	public void d1(ArrayList<List> listAry) {		
+		c1 = getContentPane();
+		c1.setLayout(null);
+		
 		// ScrollPane for Table
 		scrollPane = new JScrollPane();
 		scrollPane.setBounds(0, 80, 1013, 461);
-		getContentPane().add(scrollPane);
+		c1.add(scrollPane);
 
 		// Table
 		table = new JTable();
@@ -138,60 +181,41 @@ public class GUI extends JFrame {
 			model.setValueAt(false, i, 3);
 		}
 		
-		/** JCombo test
-		String[] dataItem = { "Item 1", "Item 2", "Item 3", "Item 4" };
-		JComboBox countryCombo = new JComboBox(dataItem);
-		TableColumn countryColumn = table.getColumnModel().getColumn(1);
-		countryColumn.setCellEditor(new DefaultCellEditor(countryCombo));
-		*/
-		
 		lastItem.setBounds(5,-10,200,100);
 		lastItem.setBorderPainted(false);
 		lastItem.setContentAreaFilled(false);
 		lastItem.setFocusPainted(false);
-		getContentPane().add(lastItem);
+		c1.add(lastItem);
 		
 		addList.setBounds(650,-10,200,100);
 		addList.setBorderPainted(false);
 		addList.setContentAreaFilled(false);
 		addList.setFocusPainted(false);
-		getContentPane().add(addList);
+		c1.add(addList);
 		
 		today.setBounds(260,-10,200,100);
 		today.setBorderPainted(false);
 		today.setContentAreaFilled(false);
 		today.setFocusPainted(false);
-		getContentPane().add(today);
+		c1.add(today);
 		
 		bookMark.setBounds(470,0,80,80);
 		bookMark.setBorderPainted(false);
 		bookMark.setContentAreaFilled(false);
 		bookMark.setFocusPainted(false);
-		getContentPane().add(bookMark);
+		c1.add(bookMark);
 		
 		trash.setBounds(900,0,80,80);
 		trash.setBorderPainted(false);
 		trash.setContentAreaFilled(false);
 		trash.setFocusPainted(false);
-		getContentPane().add(trash);
-		
-		AddListListener addListListener = new AddListListener();
-		addList.addMouseListener(addListListener);
+		c1.add(trash);
 		
 		ModifyListener modifyListener = new ModifyListener();
 		table.addMouseListener(modifyListener);
 		
-		TodayListener todayListener = new TodayListener();
-		today.addMouseListener(todayListener);
-		
-		BookMarkListener bookMarkListener = new BookMarkListener();
-		bookMark.addMouseListener(bookMarkListener);
-		
 		GoToListListener goToListListener = new GoToListListener();
 		table.addMouseListener(goToListListener);
-		
-		TrashListener trashListener = new TrashListener();
-		trash.addMouseListener(trashListener);
 		
 	}
 		
@@ -320,7 +344,11 @@ public class GUI extends JFrame {
 			bookMark.setVisible(false);
 			trash.setVisible(false);
 			addItem.setVisible(true);
+			trash2.setVisible(true);
 			home.setVisible(true);
+			print.setVisible(true);
+			dup.setVisible(true);
+			move.setVisible(true);
 			listIdx = 1;
 			d2(listIdx);
 		}
@@ -355,7 +383,11 @@ public class GUI extends JFrame {
 			bookMark.setVisible(false);
 			trash.setVisible(false);
 			addItem.setVisible(true);
+			trash2.setVisible(true);
 			home.setVisible(true);
+			print.setVisible(true);
+			dup.setVisible(true);
+			move.setVisible(true);
 			listIdx = 0;
 			d2(listIdx);
 		}
@@ -394,6 +426,10 @@ public class GUI extends JFrame {
 				trash.setVisible(false);
 				addItem.setVisible(true);
 				home.setVisible(true);
+				trash2.setVisible(true);
+				print.setVisible(true);
+				dup.setVisible(true);
+				move.setVisible(true);
 				listIdx = row + 2;
 				d2(listIdx);
 			}
@@ -411,7 +447,7 @@ public class GUI extends JFrame {
 	}
 	
 	private class TrashListener extends JFrame implements MouseListener {
-
+		
 		@Override
 		public void mouseClicked(MouseEvent e) {}
 
@@ -423,6 +459,7 @@ public class GUI extends JFrame {
 					CheckBoxAry.add(i);
 			}
 			if(CheckBoxAry.size() > 0) {
+				System.out.println(CheckBoxAry.toString());
 				JFrame f = new JFrame();
 				f.setTitle("Delete List");
 				f.setSize(300, 190);
@@ -455,7 +492,6 @@ public class GUI extends JFrame {
 				b.setBounds(110,90,60,35);
 				f.add(b);	
 			} else JOptionPane.showMessageDialog(null, "체크박스를 선택하지 않았습니다!");
-			
 		}
 
 		@Override
@@ -474,11 +510,12 @@ public class GUI extends JFrame {
 	}
 	
 	public void d2(int row) {
-	
+		c2 = getContentPane();
+		c2.setLayout(null);
 		// ScrollPane for Table
 		scrollPane = new JScrollPane();
 		scrollPane.setBounds(0, 80, 1013, 461);
-		getContentPane().add(scrollPane);
+		c2.add(scrollPane);
 
 		// Table
 		table = new JTable();
@@ -531,31 +568,40 @@ public class GUI extends JFrame {
 		addItem.setBorderPainted(false);
 		addItem.setContentAreaFilled(false);
 		addItem.setFocusPainted(false);
-		getContentPane().add(addItem);
+		c2.add(addItem);
 		
 		home.setBounds(800,0,80,80);
 		home.setBorderPainted(false);
 		home.setContentAreaFilled(false);
 		home.setFocusPainted(false);
-		getContentPane().add(home);
+		c2.add(home);
 		
 		trash2.setBounds(900,0,80,80);
 		trash2.setBorderPainted(false);
 		trash2.setContentAreaFilled(false);
 		trash2.setFocusPainted(false);
-		getContentPane().add(trash2);
+		c2.add(trash2);
 		
-		AddItemListener addItemListener = new AddItemListener();
-		addItem.addMouseListener(addItemListener);
+		print.setBounds(20,0,80,80);
+		print.setBorderPainted(false);
+		print.setContentAreaFilled(false);
+		print.setFocusPainted(false);
+		c2.add(print);
 		
+		dup.setBounds(380,-10,200,100);
+		dup.setBorderPainted(false);
+		dup.setContentAreaFilled(false);
+		dup.setFocusPainted(false);
+		c2.add(dup);
+		
+		move.setBounds(150,-10,200,100);
+		move.setBorderPainted(false);
+		move.setContentAreaFilled(false);
+		move.setFocusPainted(false);
+		c2.add(move);
+	
 		ModifyItemListener modifyItemListener = new ModifyItemListener();
 		table.addMouseListener(modifyItemListener);
-		
-		HomeListener homeListener = new HomeListener();
-		home.addMouseListener(homeListener);
-		
-		Trash2Listener trash2Listener = new Trash2Listener();
-		trash2.addMouseListener(trash2Listener);
 	}
 	
 	private class AddItemListener extends JFrame implements MouseListener {
@@ -595,19 +641,29 @@ public class GUI extends JFrame {
 					String nameData = tf1.getText();
 					String qtyData = tf2.getText();
 					try {
-						Item i = new Item(nameData,Integer.parseInt(qtyData));
-						itemIO.InputItem(i,listAry.get(listIdx).ary);
-						if (itemIO.b == true) {
-							model.addRow(new Object[0]);
-							model.setValueAt("      " + listAry.get(listIdx).ary.get(listAry.get(listIdx).ary.size() - 1).name, listAry.get(listIdx).ary.size() - 2, 0);
-							model.setValueAt("    " + listAry.get(listIdx).ary.get(listAry.get(listIdx).ary.size() - 1).num, listAry.get(listIdx).ary.size() - 2, 1);
-							model.setValueAt(rename, listAry.get(listIdx).ary.size() - 2, 2);
-							model.setValueAt(false, listAry.get(listIdx).ary.size() - 2, 3);
-							f.setVisible(false);
+						try {
+							if(Integer.parseInt(qtyData) < 0) {
+								Exception ex = new Exception();
+								throw ex;
+							}
+						} catch (NumberFormatException exception) {
+							JOptionPane.showMessageDialog(null, "수량에는 정수를 넣어주세요!");
+						} catch (Exception exception) {
+							JOptionPane.showMessageDialog(null, "음수를 넣지 말아주세요!");
 						}
-					} catch (Exception exception) {
-						JOptionPane.showMessageDialog(null, "수량에는 정수를 넣어주세요!");
-					}
+						if(Integer.parseInt(qtyData) > 0) {
+							Item i = new Item(nameData,Integer.parseInt(qtyData));
+							itemIO.InputItem(i,listAry.get(listIdx).ary);
+							if (itemIO.b == true) {
+								model.addRow(new Object[0]);
+								model.setValueAt("      " + listAry.get(listIdx).ary.get(listAry.get(listIdx).ary.size() - 1).name, listAry.get(listIdx).ary.size() - 2, 0);
+								model.setValueAt("    " + listAry.get(listIdx).ary.get(listAry.get(listIdx).ary.size() - 1).num, listAry.get(listIdx).ary.size() - 2, 1);
+								model.setValueAt(rename, listAry.get(listIdx).ary.size() - 2, 2);
+								model.setValueAt(false, listAry.get(listIdx).ary.size() - 2, 3);
+								f.setVisible(false);
+							}
+						}
+					} catch (NumberFormatException exception) {} 
 				}
 			});
 			b.setBounds(110,90,60,35);
@@ -702,10 +758,16 @@ public class GUI extends JFrame {
 			addList.setVisible(true);
 			today.setVisible(true);
 			bookMark.setVisible(true);
+			trash.setVisible(true);
+			c1.setEnabled(true);
 			scrollPane.setVisible(false);
 			table.setVisible(false);
 			addItem.setVisible(false);
+			trash2.setVisible(false);
 			home.setVisible(false);
+			print.setVisible(false);
+			dup.setVisible(false);
+			move.setVisible(false);
 			d1(listAry);
 		}
 
@@ -751,23 +813,27 @@ public class GUI extends JFrame {
 				la2.setBounds(89,38,120,60);
 				f.add(la2);
 				JButton b = new JButton("확인");
-				b.addActionListener(new ActionListener() {
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						int tmp = 1;
-						for(int i = 0 ; i < CheckBoxAry.size() ; i++) {
-							listAry.get(listIdx).ary.remove(CheckBoxAry.get(i) + tmp);	
-							tmp--; 
+				try {
+					b.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							int tmp = 1;
+							for(int i = 0 ; i < CheckBoxAry.size() ; i++) {
+								listAry.get(listIdx).ary.remove(CheckBoxAry.get(i) + tmp);	
+								tmp--; 
+							}
+							JOptionPane.showMessageDialog(null, "삭제되었습니다!");
+							f.setVisible(false);
+							table.setVisible(false);
+							scrollPane.setVisible(false);
+							UpdateTable2();
 						}
-						JOptionPane.showMessageDialog(null, "삭제되었습니다!");
-						f.setVisible(false);
-						table.setVisible(false);
-						scrollPane.setVisible(false);
-						UpdateTable2();
-					}
-				});
-				b.setBounds(110,90,60,35);
-				f.add(b);	
+					});
+					b.setBounds(110,90,60,35);
+					f.add(b);	
+				} catch (Exception except) {
+					f.setVisible(false);
+				}
 			} else JOptionPane.showMessageDialog(null, "체크박스를 선택하지 않았습니다!");
 			
 		}
@@ -784,6 +850,81 @@ public class GUI extends JFrame {
 		public void mouseExited(MouseEvent e) {
 			trash2.setIcon(trash2Exit);
 		}
+	}
+	
+	private class MoveListener extends JFrame implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent e) {}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			ArrayList<Integer> CheckBoxAry = new ArrayList<>();
+			for(int i = 0 ; i < listAry.get(listIdx).ary.size() - 1 ; i++) {
+				if ((boolean)table.getValueAt(i,3) == true) 
+					CheckBoxAry.add(i);
+			}
+			if(CheckBoxAry.size() > 0) {
+				JFrame f = new JFrame();
+				f.setTitle("Move Item");
+				f.setSize(300, 190);
+				f.setLocationRelativeTo(null);
+				f.setResizable(false);
+				f.setVisible(true);
+				f.setLayout(null);
+				String[] listName = new String[listAry.size()];
+				for(int i = 0 ; i < listAry.size() ; i++)
+					listName[i] = listAry.get(i).name;
+				JComboBox cBox = new JComboBox(listName);
+				cBox.setBounds(40, 30, 204, 20);
+				f.add(cBox);
+				cBox.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						JComboBox cb = (JComboBox)e.getSource();							
+						listNameField = (String)cb.getSelectedItem();
+						for(int i = 0 ; i < listAry.size() ; i++) {
+							if (listNameField.equals(listAry.get(i).name)) {
+								listIdx2 = i;
+								System.out.println(listIdx2);
+							}
+						}
+					}
+				});
+				JButton b = new JButton("확인");
+				b.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						int tmp = 1;
+						for(int i = 0 ; i < CheckBoxAry.size() ; i++) {
+							MoveItem moveItem = new MoveItem();
+							moveItem.Move(listAry.get(listIdx).ary.get(CheckBoxAry.get(i) + tmp), listAry.get(listIdx).ary, listAry.get(listIdx2).ary);
+							tmp--;
+						}
+						f.setVisible(false);
+						table.setVisible(false);
+						scrollPane.setVisible(false);
+						UpdateTable2();
+					}
+				});
+				b.setBounds(110,90,60,35);
+				f.add(b);	
+			} else JOptionPane.showMessageDialog(null, "체크박스를 선택하지 않았습니다!");
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			move.setIcon(moveEnter);
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			move.setIcon(moveExit);
+		}
+		
 	}
 	
 	public void UpdateTable() {
--- MoveItem.java
+++ MoveItem.java
@@ -6,9 +6,6 @@ import javax.swing.JOptionPane;
 public class MoveItem {
 	
 	public void Move(Item item, ArrayList<Item> ary1, ArrayList<Item> ary2) { //ary1의 name이름을 가진 Item이 ary2의 끝으로 이동
-		if(ary1.contains(item)) {
-			if(!ary2.contains(item)) {ary1.remove(item); ary2.add(item);} 
-			else JOptionPane.showMessageDialog(null, "Items Not Included!"); 
-		} else JOptionPane.showMessageDialog(null, "Items Already Included!");
+		ary1.remove(item); ary2.add(item);
 	}
 }
